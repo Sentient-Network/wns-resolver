5b997734b6d938c9d1fe1d82495d0894d7b6a995
mdavid <matt@staff.gid.net>
2015-02-20 17:02:20 -0800

    - wnsresolver module initial commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# wns-resolver
+# WNS Resolver
+
+Netki Wallet Name Resolver Library
+
+# General
+
+The Netki Wallet Name Resolver library allows you to quickly integrate the Wallet Name standard into your digital
+currency platform. Using the Wallet Name Service allows you to avoid difficult Bitcoin, Dogecoin, Litecoin, etc. wallet addresses
+and instead use a much more memorable naming scheme that runs on top of DNS using DNSSEC to keep the Chain-of-Trust unbroken.
+
+This library will continue to evolve as the Wallet Name Service starts to include the BIP-32 and BIP-70 specs.
+
+# Requirements
+
+## Python Modules
+- [dnspython](http://www.dnspython.org) - http://www.dnspython.org
+- [requests](http://docs.python-requests.org/en/latest/) - http://docs.python-requests.org/en/latest/
+- [pyUnbound](https://www.unbound.net/documentation/pyunbound/) - https://www.unbound.net/documentation/pyunbound/
+
+**NOTE:** pyUnbound is **NOT** available via pip. Please follow the instructions below to install and setup PyUnbound
+
+# PyUnbound Setup
+This version of **wns-resolver** has been tested with Unbound v1.4.22. ([https://unbound.net/downloads/unbound-1.4.22.tar.gz](https://unbound.net/downloads/unbound-1.4.22.tar.gz))
+
+When ./configure-ing unbound, make sure to use the **--with-pyunbound** flag. This will make pyunbound available after make and make install
+
+Please refer to [https://www.unbound.net/documentation/pyunbound/install.html](https://www.unbound.net/documentation/pyunbound/install.html) for Unbound installation help.
+
+Use the [unbound-anchor](https://www.unbound.net/documentation/unbound-anchor.html) tool to setup the ICANN-supplied DNSSEC Root Trust Anchor.
+
+# Usage
+
+**wnsresolver** provides the wnsresolver.WalletNameResolver class which has the resolve_wallet_name(name, currency) function available. 
+This can be used to resolve any Wallet Name. If our [bcresolver](https://github.com/netkicorp/blockchain-resolver) module is also installed, the WalletNameResolver will handle
+both ICANN and Namecoin Blockchain-based domains.
+
+## Success Example
+
+    >>> from wnsresolver import WalletNameResolver
+    >>> wns_resolver = WalletNameResolver()
+    >>> wns_resolver.resolve('wallet.justinnewton.me', 'btc')
+    1P5faasXEt4BVgMaQjVo6TmvFXdGgZ8FF9
+
+## Wallet Name Not Found
+
+    >>> from wnsresolver import WalletNameResolver
+    >>> wns_resolver = WalletNameResolver()
+    >>> wns_resolver.resolve_wallet_name('really-notfound.justinnewton.me','btc')
+    Traceback (most recent call last):
+      File "<input>", line 1, in <module>
+      File "/Users/mdavid/PycharmProjects/wns-resolver/wnsresolver/__init__.py", line 67, in resolve_wallet_name
+        raise WalletNameUnavailableError
+    WalletNameUnavailableError
+
+
---
+++ tests/test_walletnameresolver.py
@@ -0,0 +1,157 @@
+__author__ = 'mdavid'
+
+from mock import *
+from unittest import TestCase
+from wnsresolver import *
+
+class TestInit(TestCase):
+
+    def test_all_args(self):
+
+        wns_resolver = WalletNameResolver(
+            resolv_conf='resolv.conf',
+            dnssec_root_key='root_key',
+            nc_host = '127.0.0.1',
+            nc_port = 1234,
+            nc_rpcuser='rpcuser',
+            nc_rpcpassword='rpcpassword',
+            nc_tmpdir='/tmp'
+        )
+
+        self.assertEqual('resolv.conf', wns_resolver.resolv_conf)
+        self.assertEqual('root_key', wns_resolver.dnssec_root_key)
+        self.assertEqual('127.0.0.1', wns_resolver.nc_host)
+        self.assertEqual(1234, wns_resolver.nc_port)
+        self.assertEqual('rpcuser', wns_resolver.nc_user)
+        self.assertEqual('rpcpassword', wns_resolver.nc_password)
+        self.assertEqual('/tmp', wns_resolver.nc_tmpdir)
+
+    def test_defaults(self):
+
+        wns_resolver = WalletNameResolver()
+
+        self.assertEqual('/etc/resolv.conf', wns_resolver.resolv_conf)
+        self.assertEqual('/usr/local/etc/unbound/root.key', wns_resolver.dnssec_root_key)
+        self.assertIsNone(wns_resolver.nc_host)
+        self.assertEqual(8336, wns_resolver.nc_port)
+        self.assertIsNone(wns_resolver.nc_user)
+        self.assertIsNone(wns_resolver.nc_password)
+        self.assertIsNone(wns_resolver.nc_tmpdir)
+
+class TestNamecoinOptions(TestCase):
+
+    def test_with_args(self):
+
+        wns_resolver = WalletNameResolver()
+
+        wns_resolver.set_namecoin_options(
+            host='127.0.0.1',
+            port=1234,
+            user='rpcuser',
+            password='rpcpassword',
+            tmpdir='/tmp'
+        )
+
+        self.assertEqual('/etc/resolv.conf', wns_resolver.resolv_conf)
+        self.assertEqual('/usr/local/etc/unbound/root.key', wns_resolver.dnssec_root_key)
+        self.assertEqual('127.0.0.1', wns_resolver.nc_host)
+        self.assertEqual(1234, wns_resolver.nc_port)
+        self.assertEqual('rpcuser', wns_resolver.nc_user)
+        self.assertEqual('rpcpassword', wns_resolver.nc_password)
+        self.assertEqual('/tmp', wns_resolver.nc_tmpdir)
+
+    def test_defaults(self):
+
+        wns_resolver = WalletNameResolver(nc_host='127.0.0.1')
+        wns_resolver.set_namecoin_options()
+
+        self.assertEqual('/etc/resolv.conf', wns_resolver.resolv_conf)
+        self.assertEqual('/usr/local/etc/unbound/root.key', wns_resolver.dnssec_root_key)
+        self.assertIsNone(wns_resolver.nc_host)
+        self.assertEqual(8336, wns_resolver.nc_port)
+        self.assertIsNone(wns_resolver.nc_user)
+        self.assertIsNone(wns_resolver.nc_password)
+        self.assertIsNone(wns_resolver.nc_tmpdir)
+
+class TestResolveWalletName(TestCase):
+
+    def setUp(self):
+
+        self.patcher1 = patch('wnsresolver.WalletNameResolver.resolve')
+        self.patcher2 = patch('bcresolver.NamecoinResolver')
+
+        self.mockWnsResolver = self.patcher1.start()
+        self.mockNamecoinResolver = self.patcher2.start()
+
+        self.mockWnsResolver.side_effect = [
+            'btc',
+            '23456789MgDBffBffBff'
+        ]
+
+        self.mockNamecoinResolver.return_value.resolve.side_effect = [
+            'btc',
+            '23456789MgDBffBffBff'
+        ]
+
+    def tearDown(self):
+
+        self.patcher1.stop()
+        self.patcher2.stop()
+        
+    def test_go_right(self):
+        
+        wns_resolver = WalletNameResolver()
+        ret_val = wns_resolver.resolve_wallet_name('wallet.mattdavid.xyz', 'btc')
+
+        self.assertEqual('23456789MgDBffBffBff', ret_val)
+        self.assertEqual(2, self.mockWnsResolver.call_count)
+
+    def test_no_name(self):
+
+        wns_resolver = WalletNameResolver()
+        self.assertRaises(AttributeError, wns_resolver.resolve_wallet_name, None, 'btc')
+        self.assertEqual(0, self.mockWnsResolver.call_count)
+
+    def test_no_currency(self):
+
+        wns_resolver = WalletNameResolver()
+        self.assertRaises(AttributeError, wns_resolver.resolve_wallet_name, 'wallet.mattdavid.xyz', None)
+        self.assertEqual(0, self.mockWnsResolver.call_count)
+
+    def test_no_currency_list(self):
+
+        wns_resolver = WalletNameResolver()
+        self.assertRaises(WalletNameCurrencyUnavailableError, wns_resolver.resolve_wallet_name, 'wallet.mattdavid.xyz', 'dgc')
+        self.assertEqual(1, self.mockWnsResolver.call_count)
+
+    def test_no_available_currency(self):
+
+        self.mockWnsResolver.side_effect = None
+        self.mockWnsResolver.return_value = None
+
+        wns_resolver = WalletNameResolver()
+        self.assertRaises(WalletNameUnavailableError, wns_resolver.resolve_wallet_name, 'wallet.mattdavid.xyz', 'btc')
+        self.assertEqual(1, self.mockWnsResolver.call_count)
+
+    def test_namecoin_go_right(self):
+
+        wns_resolver = WalletNameResolver()
+        ret_val = wns_resolver.resolve_wallet_name('wallet.mattdavid.bit', 'btc')
+
+        self.assertEqual('23456789MgDBffBffBff', ret_val)
+        self.assertEqual(1, self.mockNamecoinResolver.call_count)
+        self.assertEqual(2, self.mockNamecoinResolver.return_value.resolve.call_count)
+
+        self.assertEqual(wns_resolver.nc_host, self.mockNamecoinResolver.call_args[0][0])
+        self.assertEqual(wns_resolver.nc_user, self.mockNamecoinResolver.call_args[0][1])
+        self.assertEqual(wns_resolver.nc_password, self.mockNamecoinResolver.call_args[0][2])
+        self.assertEqual(wns_resolver.nc_port, self.mockNamecoinResolver.call_args[0][3])
+        self.assertEqual(wns_resolver.nc_tmpdir, self.mockNamecoinResolver.call_args[0][4])
+
+    def test_namecoin_import_error(self):
+
+        self.mockNamecoinResolver.side_effect = ImportError()
+
+        wns_resolver = WalletNameResolver()
+        self.assertRaises(WalletNameNamecoinUnavailable, wns_resolver.resolve_wallet_name, 'wallet.mattdavid.bit', 'btc')
+        self.assertEqual(1, self.mockNamecoinResolver.call_count)
