1420accfa502b97c4580c8d13a466b3a818c7c97
Frank Contreras <frank@staff.gid.net>
2015-03-19 19:29:04 -0700

    Increment version to 0.0.3 with real LICENSE file fix

--- setup.py
+++ setup.py
@@ -12,19 +12,18 @@ test_requires = [
 
 setup(
     name='wnsresolver',
-    version='0.0.2',
+    version='0.0.3',
     packages=['wnsresolver'],
     install_requires=install_requires,
     tests_require=test_requires,
     test_suite = 'tests',
     url='https://github.com/netkicorp/wns-resolver',
-    download_url='https://github.com/netkicorp/wns-resolver/tarball/0.0.1',
+    download_url='https://github.com/netkicorp/wns-resolver/tarball/0.0.3',
     platforms=['any'],
     license='BSD',
     author='mdavid',
     author_email='opensource@netki.com',
-    description='Wallet Name Resolver Library',
-    data_files = [('', ['LICENSE'])]
+    description='Wallet Name Resolver Library'
 )
 
 print('Requirement Unbound is not available via pip. Please see README.md for installation information')
